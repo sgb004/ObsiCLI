02eeba217390d77d5c56a5c8be84fbf31a75b286
sgb004 <sgb004@gmail.com>
2025-02-03 18:35:37 -0600

    Added methods output and input

--- obsicl/scripts.js
+++ obsicl/scripts.js
@@ -1,5 +1,71 @@
-const obsiCL = () => {
-	console.log('obsiCL');
-};
+class ObsiCL extends HTMLElement {
+	#output;
+	#inCallbacks;
 
-export default obsiCL;
+	constructor() {
+		super();
+
+		this.innerHTML = `
+			<div class="screen">
+				<div class="output">
+				</div>
+				<form class="input">
+					<input type="text" value="" />
+					<input type="submit" value="" />
+				</form>
+			</div>
+		`;
+
+		this.#output = this.querySelector('.output');
+		this.#inCallbacks = {
+			resolve: () => {},
+			reject: () => {},
+		};
+		this.#addListeners();
+	}
+
+	#addListeners() {
+		const form = this.querySelector('.input');
+		if (form instanceof HTMLFormElement) {
+			form.addEventListener('submit', this.#handleInputSubmit.bind(this));
+		}
+	}
+
+	#handleInputSubmit(event) {
+		event.preventDefault();
+
+		const input = event.currentTarget.querySelector('input[type="text"]');
+
+		if (input instanceof HTMLInputElement) {
+			this.ou(input.value);
+			this.#inCallbacks.resolve(input.value);
+			event.currentTarget.reset();
+		} else {
+			this.#inCallbacks.reject(input.value);
+		}
+
+		this.#inCallbacks = {
+			resolve: () => {},
+			reject: () => {},
+		};
+	}
+
+	ou(message) {
+		const line = document.createElement('pre');
+		line.textContent = message;
+
+		this.#output.appendChild(line);
+	}
+
+	in() {
+		const cl = this;
+		return new Promise((resolve, reject) => {
+			this.#inCallbacks = {
+				resolve,
+				reject,
+			};
+		});
+	}
+}
+
+customElements.define('obsi-cl', ObsiCL);
